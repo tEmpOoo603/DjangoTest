c01969d8c55ffadbada0ed6e9cea0436addb9f9e
Efim laptop <efimaniskin@gmail.com>
2024-07-17 12:52:43 +0300

    Add about page, work with tags

--- app/main/views.py
+++ app/main/views.py
@@ -4,16 +4,14 @@ from django.shortcuts import render
 
 def index(request):
     user_info = {
-        'is_authenticated':False,
-        'name':'Efim',
-        'pass':'123',
-        'cars':['BMW','AUDI','MERCEDES'],
-        'parents':{'mom':'Tatiana'},
+        'title':'Home_Made',
+        'content':'Магазин мебели HOME_MADE',
     }
     return render(request, 'main/index.html', context=user_info)
 def about(request):
     data = {
         'title':'О нас',
         'content':'Здесь вы найдётся информацию о нас!',
+        'text_on_page':'Дополнительная информация о нашем магазине и товарах',
     }
     return render(request, 'main/about.html', context=data)
