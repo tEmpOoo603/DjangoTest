03df20a3f0d4c5ace68c6bd8e8b2beddd1ed4c92
Efim Aniskin <efimaniskin@gmail.com>
2024-07-27 18:55:44 +0300

    Realize filters on "all" page(error when select category), also error when switch page

--- app/goods/views.py
+++ app/goods/views.py
@@ -5,10 +5,17 @@ from goods.models import Products, Categories
 
 
 def catalog(request, category_slug):
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+
     if category_slug == 'all':
         goods = Products.objects.all().select_related('category')
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug).select_related('category'))
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by)
     page = request.GET.get('page',1)
     paginator = Paginator(goods, 3)
     curr_page = paginator.page(int(page))
