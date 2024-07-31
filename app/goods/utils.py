686584ed29aa2cd0e9c6a89cec898c50e2aa316d
Efim Aniskin <efimaniskin@gmail.com>
2024-07-31 22:45:47 +0300

    Now use main algorithm, using ranks(SearchVector, SearchQuery,SearchRank)

--- app/goods/utils.py
+++ app/goods/utils.py
@@ -1,12 +1,14 @@
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from goods.models import Products
 
 def q_search(query:str):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
 
-    return Products.objects.annotate(search=SearchVector('name','description')).filter(search=query)
+    vector = SearchVector('name','description')
+    query = SearchQuery(query)
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
 
     # return Products.objects.filter(description__search=query)
 
