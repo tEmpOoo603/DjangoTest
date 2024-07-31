a19bade511cca7f9265db5d9366dbe9dc0554b85
Efim Aniskin <efimaniskin@gmail.com>
2024-07-31 22:32:36 +0300

    Comment old searching algorithm, now it use postgres search lookup __search

--- app/goods/utils.py
+++ app/goods/utils.py
@@ -6,10 +6,12 @@ def q_search(query:str):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
 
-    q_objects = Q()
-    keywords = [ word for word in query.split() if len(word)>2 ]
+    return Products.objects.filter(description__search=query)
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
-        return Products.objects.filter(q_objects)
+    # q_objects = Q()
+    # keywords = [ word for word in query.split() if len(word)>2 ]
+    #
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+    #     return Products.objects.filter(q_objects)
