21e34272d38402c097c27fbe62887471e304e979
Efim Aniskin <efimaniskin@gmail.com>
2024-07-31 22:37:44 +0300

    Comment old searching algorithm, now use SearchVector for name and description

--- app/goods/utils.py
+++ app/goods/utils.py
@@ -1,12 +1,14 @@
 from django.db.models import Q
-
+from django.contrib.postgres.search import SearchVector
 from goods.models import Products
 
 def q_search(query:str):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
 
-    return Products.objects.filter(description__search=query)
+    return Products.objects.annotate(search=SearchVector('name','description')).filter(search=query)
+
+    # return Products.objects.filter(description__search=query)
 
     # q_objects = Q()
     # keywords = [ word for word in query.split() if len(word)>2 ]
