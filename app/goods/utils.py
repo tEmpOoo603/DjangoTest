b8f489a1609d2622b5ed9fbd032f398d979d71e2
Efim Aniskin <efimaniskin@gmail.com>
2024-07-31 14:54:19 +0300

    Add searching by keywords, update q_search function

--- app/goods/utils.py
+++ app/goods/utils.py
@@ -1,5 +1,15 @@
+from django.db.models import Q
+
 from goods.models import Products
 
-def q_search(query):
+def q_search(query:str):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+
+    q_objects = Q()
+    keywords = [ word for word in query.split() if len(word)>2 ]
+
+    for token in keywords:
+        q_objects |= Q(description__icontains=token)
+        q_objects |= Q(name__icontains=token)
+        return Products.objects.filter(q_objects)
