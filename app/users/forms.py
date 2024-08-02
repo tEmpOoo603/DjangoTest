5f7ebaf665bd4d82ca5de1c655ecde9f411051f0
Efim Aniskin <efimaniskin@gmail.com>
2024-08-03 01:28:23 +0300

    Main way to create auth on site, using custom forms, right template and view foo

--- app/users/forms.py
+++ app/users/forms.py
@@ -5,24 +5,7 @@ from django import forms
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={"autofocus": True,
-                                                             "type": "text",
-                                                             "class": "form-control",
-                                                             "placeholder": "Введите ваше имя пользователя",
-                                                             "required": True
-                                                             }
-                                                      ),
-                               label="Имя пользователя"
-                               )
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
-                                                                 "type": "password",
-                                                                 "class": "form-control",
-                                                                 "placeholder": "Введите ваш пароль",
-                                                                 "required": True
-                                                                 }
-                                                          ),
-                               label="Пароль"
-                               )
-
+    username = forms.CharField()
+    password = forms.CharField()
     class Meta:
         model = User
