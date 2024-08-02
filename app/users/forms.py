abd7ea77d67deafbaa36a798e0550038e3627957
Efim Aniskin <efimaniskin@gmail.com>
2024-08-03 02:06:32 +0300

    Add error display, add html file with notifications, use include tag in base html to display error message

--- app/users/forms.py
+++ app/users/forms.py
@@ -1,11 +1,8 @@
 from django.contrib.auth.forms import AuthenticationForm
 
-from users.models import User
-from django import forms
+from users.models import Users
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField()
-    password = forms.CharField()
     class Meta:
         model = User
