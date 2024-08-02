aa3bcda40f84e4cf3b0b0d4ac94aa1260cd82094
Efim Aniskin <efimaniskin@gmail.com>
2024-08-02 23:10:06 +0300

    Redefine username, password from AuthenticationForm, update login template to work with it

--- app/users/forms.py
+++ app/users/forms.py
@@ -3,6 +3,26 @@ from django.contrib.auth.forms import AuthenticationForm
 from users.models import User
 from django import forms
 
+
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={"autofocus": True,
+                                                             "type": "text",
+                                                             "class": "form-control",
+                                                             "placeholder": "Введите ваше имя пользователя",
+                                                             "required": True
+                                                             }
+                                                      ),
+                               label="Имя пользователя"
+                               )
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
+                                                                 "type": "password",
+                                                                 "class": "form-control",
+                                                                 "placeholder": "Введите ваш пароль",
+                                                                 "required": True
+                                                                 }
+                                                          ),
+                               label="Пароль"
+                               )
+
     class Meta:
-        models = User
+        model = User
