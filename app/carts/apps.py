112287a72a53d1ed75cf1d0e75bcfacc51ff1013
Efim Aniskin <efimaniskin@gmail.com>
2024-08-04 22:36:59 +0300

    Create new model Cart, register it

--- app/carts/apps.py
+++ app/carts/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CartsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'carts'
+    verbose_name = 'Корзина'
+    verbose_name_plural = 'Корзины'
+
