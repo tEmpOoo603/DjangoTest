1f25394b449a943051d973d21fcf71e2bbcb186b
Efim Aniskin <efimaniskin@gmail.com>
2024-08-05 21:49:50 +0300

    Add session support, when u un-login and add some stuff to cart, if u login or register, this stuff will adds to your cart

--- app/users/views.py
+++ app/users/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+
+from carts.models import Cart
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
 
@@ -13,9 +15,16 @@ def login(request):
             username = request.POST.get('username', None)
             password = request.POST.get('password', None)
             user = auth.authenticate(username=username, password=password)
+
+            session_key = request.session.session_key
+
             if user:
                 auth.login(request, user)
                 messages.success(request, f'{username}, Вы вошли в аккаунт')
+
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
+
                 redirect_page = request.POST.get('next', None)
                 if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
@@ -34,8 +43,15 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+
+            session_key = request.session.session_key
+
             user = form.instance
             auth.login(request, user)
+
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
+
             messages.success(request, f'{user.username}, Вы вошли в аккаунт')
             return HttpResponseRedirect(reverse('main:index'))
     else:
