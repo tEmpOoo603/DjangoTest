a0815a7d374eacec3837eb19fd9be139f4b5d0e3
Efim Aniskin <efimaniskin@gmail.com>
2024-08-07 00:39:14 +0300

    Add right render for carts, users, add user cart on user page in admin page

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
+from carts.admin import CartTabAdmin
+from carts.models import Cart
 from users.models import User
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'first_name', 'last_name', 'email',]
+    search_fields = ['username', 'first_name', 'last_name', 'email',]
 
-admin.site.register(User)
+    inlines = [CartTabAdmin,]
