39f96473d0d18c13ca93ba98955e9be633d05159
Efim Aniskin <efimaniskin@gmail.com>
2024-08-07 13:08:07 +0300

    Add functions to Carts.admin to display user/anonim carts, right product name

--- app/carts/admin.py
+++ app/carts/admin.py
@@ -11,5 +11,13 @@ class CartTabAdmin(admin.TabularInline):
     extra = 1
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    list_display = ['user', 'product', 'quantity', 'created_timestamp']
+    list_display = ['user_display', 'product_display', 'quantity', 'created_timestamp']
     list_filter = ['created_timestamp', 'user', 'product__name']
+
+    def user_display(self, obj):
+        if obj.user:
+            return str(obj.user)
+        return 'Анонимный пользователь'
+
+    def product_display(self, obj):
+         return str(obj.product.name)
