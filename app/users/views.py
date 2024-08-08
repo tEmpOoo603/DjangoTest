71055443d960d9112838ca0ad091750b70c8fb5e
Efim Aniskin <efimaniskin@gmail.com>
2024-08-08 20:55:07 +0300

    Now displaying orders on profile page, fix some bugs

--- app/users/views.py
+++ app/users/views.py
@@ -1,9 +1,11 @@
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
+from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
+from orders.models import Order, OrderItem
 from carts.models import Cart
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
@@ -73,9 +75,15 @@ def profile(request):
             return HttpResponseRedirect(reverse('user:profile'))
     else:
         form = ProfileForm(instance=request.user)
+
+    orders = (Order.objects.filter(user=request.user)
+              .prefetch_related(Prefetch('orderitem_set',queryset=OrderItem.objects.select_related('product')))
+              .order_by('-id'))
+
     context = {
         'form': form,
-        'title': 'Страница профиля'
+        'title': 'Страница профиля',
+        'orders':orders
     }
     return render(request, 'users/profile.html', context=context)
 
