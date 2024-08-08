4da7cf5480bbb7280c21342c78bc1abdbb6d60a9
Efim Aniskin <efimaniskin@gmail.com>
2024-08-08 13:16:09 +0300

    New models Orders and OrderItems, controller to create orders

--- app/users/admin.py
+++ app/users/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 
 from carts.admin import CartTabAdmin
 from carts.models import Cart
+from orders.admin import OrderTabularAdmin
 from users.models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'first_name', 'last_name', 'email',]
     search_fields = ['username', 'first_name', 'last_name', 'email',]
 
-    inlines = [CartTabAdmin,]
+    inlines = [CartTabAdmin, OrderTabularAdmin]
