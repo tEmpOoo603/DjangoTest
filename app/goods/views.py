294146d1d8388871cc2c17b01dc5478f71b1dd8a
Efim Aniskin <efimaniskin@gmail.com>
2024-08-09 17:13:31 +0300

    Create class CatalogView, update urls, templates

--- app/goods/views.py
+++ app/goods/views.py
@@ -1,40 +1,53 @@
 from django.core.paginator import Paginator
+from django.http import Http404
 from django.shortcuts import render, get_list_or_404
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from goods.models import Products, Categories
 from goods.utils import q_search
 
 
-def catalog(request, category_slug=None):
-    on_sale = request.GET.get('on_sale', None)
-    order_by = request.GET.get('order_by', None)
-    query = request.GET.get('q', None)
-    if category_slug == 'all':
-        goods = Products.objects.all().select_related('category')
-    elif query:
-        goods = q_search(query)
-    else:
-        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug).select_related('category'))
-    if on_sale:
-        goods = goods.filter(discount__gt=0)
-    if order_by and order_by != 'default':
-        goods = goods.order_by(order_by)
-    page = request.GET.get('page',1)
-    paginator = Paginator(goods, 3)
-    curr_page = paginator.page(int(page))
-    context = {
-        'title': 'Home - каталог',
-        'goods': curr_page,
-        'slug_url': category_slug,
-    }
-    return render(request, 'goods/catalog.html', context=context)
+class CatalogView(ListView):
+    # model = Products
+    template_name = 'goods/catalog.html'
+    context_object_name = 'goods'
+    paginate_by = 3
+    allow_empty = False
+    queryset = Products.objects.all().select_related('category')
+
+    def get_queryset(self):
+        category_slug = self.kwargs.get('category_slug')
+        on_sale = self.request.GET.get('on_sale', None)
+        order_by = self.request.GET.get('order_by', None)
+        query = self.request.GET.get('q', None)
+        if category_slug == 'all':
+            goods = super().get_queryset()
+        elif query:
+            goods = q_search(query)
+        else:
+            goods = super().get_queryset().filter(category__slug=category_slug)
+            if not goods.exists():
+                raise Http404()
+        if on_sale:
+            goods = goods.filter(discount__gt=0)
+        if order_by and order_by != 'default':
+            goods = goods.order_by(order_by)
+        return goods
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - Каталог'
+        context['slug_url'] = self.kwargs.get('category_slug')
+        return context
+
+
 
 
 class ProductView(DetailView):
     template_name = 'goods/product.html'
     slug_url_kwarg = 'product_slug'
     context_object_name = 'product'
+
     def get_object(self, queryset=None):
         product = Products.objects.get(slug=self.kwargs.get(self.slug_url_kwarg))
         return product
@@ -50,3 +63,29 @@ class ProductView(DetailView):
 #         'product':product,
 #     }
 #     return render(request, 'goods/product.html', context=context)
+
+# def catalog(request, category_slug=None):
+#     on_sale = request.GET.get('on_sale', None)
+#     order_by = request.GET.get('order_by', None)
+#     query = request.GET.get('q', None)
+#     if category_slug == 'all':
+#         goods = Products.objects.all().select_related('category')
+#     elif query:
+#         goods = q_search(query)
+#     else:
+#         goods = Products.objects.filter(category__slug=category_slug).select_related('category')
+#         if not goods.exists():
+#             raise Http404()
+#     if on_sale:
+#         goods = goods.filter(discount__gt=0)
+#     if order_by and order_by != 'default':
+#         goods = goods.order_by(order_by)
+#     page = request.GET.get('page',1)
+#     paginator = Paginator(goods, 3)
+#     curr_page = paginator.page(int(page))
+#     context = {
+#         'title': 'Home - каталог',
+#         'goods': curr_page,
+#         'slug_url': category_slug,
+#     }
+#     return render(request, 'goods/catalog.html', context=context)
