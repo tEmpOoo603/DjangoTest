200b675db7e313621f64c67a3629a5626eafcbd5
Efim Aniskin <efimaniskin@gmail.com>
2024-08-11 18:35:43 +0300

    Rework users app to, now it work with CBV-ClassBasedViews

--- app/users/urls.py
+++ app/users/urls.py
@@ -4,9 +4,9 @@ from users import views
 app_name = 'user'
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('registration/', views.registration, name='registration'),
-    path('profile/', views.profile, name='profile'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('registration/', views.UserRegistrationView.as_view(), name='registration'),
+    path('profile/', views.UserProfileView.as_view(), name='profile'),
     path('logout/', views.logout, name='logout'),
-    path('user-cart/', views.user_cart, name='user_cart'),
+    path('user-cart/', views.UserCartView.as_view(), name='user_cart'),
 ]
